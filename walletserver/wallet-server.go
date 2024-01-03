0f39feb5aa5211afd9c0fc96067ba92da108c3f7
feliux <ahffro@gmail.com>
2024-01-03 22:12:48 +0000

    [feat] return total amount from wallet

--- walletserver/wallet-server.go
+++ walletserver/wallet-server.go
@@ -3,6 +3,7 @@ package walletserver
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"io"
 	"log"
@@ -10,6 +11,7 @@ import (
 	"path"
 	"strconv"
 
+	"github.com/feliux/blkchn/blockchain"
 	"github.com/feliux/blkchn/transaction"
 	"github.com/feliux/blkchn/utils"
 	"github.com/feliux/blkchn/wallet"
@@ -100,11 +102,11 @@ func (ws *WalletServer) CreateTransaction(w http.ResponseWriter, req *http.Reque
 			&value32,
 			&signatureStr,
 		}
-		m, err := json.Marshal(bt)
+		response, err := json.Marshal(bt)
 		if err != nil {
 			log.Printf("ERROR marshaling data: %s" + err.Error())
 		}
-		buf := bytes.NewBuffer(m)
+		buf := bytes.NewBuffer(response)
 		w.Header().Add("Content-Type", "application/json")
 		resp, err := http.Post(ws.Gateway()+"/transactions", "application/json", buf)
 		if err != nil {
@@ -121,9 +123,57 @@ func (ws *WalletServer) CreateTransaction(w http.ResponseWriter, req *http.Reque
 	}
 }
 
+func (ws *WalletServer) WalletAmount(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		blockchainAddress := req.URL.Query().Get("blockchain_address")
+		endpoint := fmt.Sprintf("%s/amount", ws.Gateway())
+		// lets build a GET to blockchain server
+		client := &http.Client{}
+		bcsReq, _ := http.NewRequest("GET", endpoint, nil) // nil cause no body data
+		q := bcsReq.URL.Query()
+		q.Add("blockchain_address", blockchainAddress) // add param
+		bcsReq.URL.RawQuery = q.Encode()
+
+		bcsResp, err := client.Do(bcsReq) // do GET
+		if err != nil {
+			log.Printf("ERROR sending GET to blockchain server: %s" + err.Error())
+			io.WriteString(w, string(utils.JsonStatus("failed")))
+			return
+		}
+
+		w.Header().Add("Content-Type", "application/json")
+		if bcsResp.StatusCode == 200 {
+			decoder := json.NewDecoder(bcsResp.Body)
+			var bcAmountResponse blockchain.AmountResponse
+			err := decoder.Decode(&bcAmountResponse)
+			if err != nil {
+				log.Printf("ERROR decoding data from blockchain server: %s" + err.Error())
+				io.WriteString(w, string(utils.JsonStatus("failed")))
+				return
+			}
+
+			response, _ := json.Marshal(struct {
+				Message string  `json:"message"`
+				Amount  float32 `json:"amount"`
+			}{
+				Message: "success",
+				Amount:  bcAmountResponse.Amount,
+			})
+			io.WriteString(w, string(response[:]))
+		} else {
+			io.WriteString(w, string(utils.JsonStatus("failed")))
+		}
+	default:
+		log.Printf("ERROR: Invalid HTTP Method.")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
 func (ws *WalletServer) Run() {
 	http.HandleFunc("/", ws.Index)
 	http.HandleFunc("/wallet", ws.Wallet)
+	http.HandleFunc("/wallet/amount", ws.WalletAmount) // localhost:8080/wallet/amount?blockchain_address=XXX
 	http.HandleFunc("/transaction", ws.CreateTransaction)
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(ws.Port()), nil))
 }
